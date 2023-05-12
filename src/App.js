d3ab0beeff2280b875dbcca7241e6419e3320014
yyjjvv <jiwonyoo66@gmail.com>
2023-05-12 17:05:20 +0900

    🎨 feat: router 설정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// components
 import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer";
+import Main from "./pages/Main";
+import ProductsList from "./pages/ProductsList";
+import Bookmark from "./pages/Bookmark";
+
+// styles
 import "./App.css";
 
 const App = () => {
     return (
-        <div className="App">
-            <Header />
-            <Footer />
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/products/list" element={<ProductsList />} />
+                    <Route path="/bookmark" element={<Bookmark />} />
+                </Routes>
+                <Footer />
+            </div>
+        </BrowserRouter>
     );
 };
 
--- src/components/UI/DropdownMenu.js
+++ src/components/UI/DropdownMenu.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 const DropdownMenu = () => {
-    return <div>DropdownMenu</div>;
+    return (
+        <div id="menu">
+            <span>유지원님, 안녕하세요!</span>
+            <Link to="/products/list">상품리스트 페이지</Link>
+            <Link to="/bookmark">북마크 페이지</Link>
+        </div>
+    );
 };
 
 export default DropdownMenu;
