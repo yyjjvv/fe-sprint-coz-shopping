60e183e4704a0d40ee3aafdb7ed7245f2d47aeb6
yyjjvv <jiwonyoo66@gmail.com>
2023-05-13 00:13:44 +0900

    ✨ feat: DropdownMenu 컴포넌트 작성, Header 연결

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 // components
 import DropdownMenu from "../UI/DropdownMenu";
@@ -6,6 +7,7 @@ import styled from "styled-components";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 // img
 import logo from "../../assets/logo.png";
+
 export const HeaderContainer = styled.header`
     position: sticky;
     display: flex;
@@ -13,7 +15,10 @@ export const HeaderContainer = styled.header`
     align-items: center;
     height: 8rem;
     box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+    -webkit-box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+    -moz-box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
     .header-wrapper {
+        position: relative;
         display: flex;
         justify-content: space-between;
         align-items: center;
@@ -25,6 +30,7 @@ export const HeaderContainer = styled.header`
     .logo {
         display: flex;
         align-items: center;
+        cursor: pointer;
         img {
             display: block;
             width: 5.5rem;
@@ -36,22 +42,34 @@ export const HeaderContainer = styled.header`
             font-weight: 700;
         }
     }
+    .gnb-btn {
+        cursor: pointer;
+    }
 `;
+
 const Header = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    const handleToggleMenu = () => {
+        setToggleMenu((prevState) => !prevState);
+    };
+
     return (
         <HeaderContainer>
             <div className="header-wrapper">
                 <Link to="/">
-                    <h1 className="logo">
+                    <h1 className="logo" onClick={handleToggleMenu}>
                         <img src={logo} alt="logo" />
                         <span>COZ Shopping</span>
                     </h1>
                 </Link>
-                <div className="gnb-btn">
+                <div className="gnb-btn" onClick={handleToggleMenu}>
                     <Bars3Icon width={40} />
                 </div>
+                {toggleMenu && (
+                    <DropdownMenu handleToggleMenu={handleToggleMenu} />
+                )}
             </div>
-            {/* <DropdownMenu /> */}
         </HeaderContainer>
     );
 };
--- src/components/UI/DropdownMenu.js
+++ src/components/UI/DropdownMenu.js
@@ -1,12 +1,78 @@
 import { Link } from "react-router-dom";
+// dependencies
+import styled from "styled-components";
+import { GiftIcon, StarIcon } from "@heroicons/react/24/outline";
 
-const DropdownMenu = () => {
+export const Gnb = styled.nav`
+    position: absolute;
+    top: 3.5rem;
+    right: 3.2rem;
+    display: flex;
+    flex-direction: column;
+    height: 8rem;
+    .tail {
+        width: 1.6rem;
+        padding-top: 1.8rem;
+        margin-left: 13rem;
+        -webkit-clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+        clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+        background-color: #ffffff;
+        box-shadow: 0px -2px 8px 5px rgba(0, 0, 0, 0.1);
+        -webkit-box-shadow: 0px -2px 8px 5px rgba(0, 0, 0, 0.1);
+        -moz-box-shadow: 0px -2px 8px 5px rgba(0, 0, 0, 0.1);
+    }
+    ul {
+        display: flex;
+        flex-direction: column;
+        width: 20rem;
+        height: 15rem;
+        border-radius: 1.2rem;
+        background-color: #ffffff;
+        box-shadow: 0px -2px 8px 5px rgba(0, 0, 0, 0.1);
+        -webkit-box-shadow: 0px -2px 8px 5px rgba(0, 0, 0, 0.1);
+        -moz-box-shadow: 0px -2px 8px 5px rgba(0, 0, 0, 0.1);
+
+        > li {
+            display: flex;
+            align-items: center;
+            padding-left: 2.4rem;
+            height: 5rem;
+            font-size: 1.6rem;
+            &:not(:first-of-type) {
+                border-top: 0.5px solid rgba(0, 0, 0, 0.1);
+            }
+        }
+        a {
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+        }
+        svg {
+            margin-right: 0.8rem;
+        }
+    }
+`;
+
+const DropdownMenu = ({ handleToggleMenu }) => {
     return (
-        <div id="menu">
-            <span>유지원님, 안녕하세요!</span>
-            <Link to="/products/list">상품리스트 페이지</Link>
-            <Link to="/bookmark">북마크 페이지</Link>
-        </div>
+        <Gnb>
+            <div className="tail"></div>
+            <ul>
+                <li>유지원님, 안녕하세요!</li>
+                <li onClick={handleToggleMenu}>
+                    <Link to="/products/list">
+                        <GiftIcon width={20} height={20} />
+                        상품리스트 페이지
+                    </Link>
+                </li>
+                <li onClick={handleToggleMenu}>
+                    <Link to="/bookmark">
+                        <StarIcon width={20} height={20} />
+                        북마크 페이지
+                    </Link>
+                </li>
+            </ul>
+        </Gnb>
     );
 };
 
