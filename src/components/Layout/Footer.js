75e363b9ef28c55e79cfaf851dcb1b98d0062ee5
yyjjvv <jiwonyoo66@gmail.com>
2023-05-15 09:23:36 +0900

    ✨ feat: Footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+    padding: 1.1rem 0;
+    text-align: center;
+    color: #888888;
+    font-size: 1.2rem;
+`;
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <FooterContainer>
+            <div>개인정보 처리방침 | 이용 약관</div>
+            <div>All rights reserved @ Codestates</div>
+        </FooterContainer>
+    );
 };
 
 export default Footer;
